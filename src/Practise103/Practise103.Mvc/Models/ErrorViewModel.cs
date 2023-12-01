fe5c0365a860d36b2c932a0871eafdd88e7a6590
Merve <mervezgin@gmail.com>
2023-12-01 14:48:01 +0300

    initial commit

---
+++ src/Practise103/Practise103.Mvc/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Practise103.Mvc.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
