e96b090b0c0649526388fd6091f3345bfa19886a
Merve <mervezgin@gmail.com>
2023-12-02 10:11:23 +0300

    Added Entities in Core

---
+++ src/Practise103/Practise103.Core/Entity/ProductEntity.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practise103.Core.Entity
+{
+    public class ProductEntity : EntityBase
+    {
+        public string Name { get; set; }
+        public Decimal Price { get; set; }
+        public int Stock { get; set; }
+        public string UserId { get; set; }
+    }
+}
