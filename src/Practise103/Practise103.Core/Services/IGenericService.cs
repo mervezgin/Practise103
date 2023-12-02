a753c68b2d784e18385e83c3a0a1cd157e289e62
Merve <mervezgin@gmail.com>
2023-12-02 16:05:29 +0300

    Added IGenericService.cs in Core

---
+++ src/Practise103/Practise103.Core/Services/IGenericService.cs
@@ -0,0 +1,20 @@
+﻿using Practise103.Shared.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practise103.Core.Services
+{
+    public interface IGenericService<TEntity, TDto> where TEntity : class where TDto : class 
+    {
+        Task<ResponseDto<TDto>> GetByIdAsync(int id);
+        Task<ResponseDto<IEnumerable<TDto>>> GetAllAsync();
+        Task<ResponseDto<IEnumerable<TDto>>> Where(Expression<Func<TEntity, bool>> predicate);
+        Task<ResponseDto<TDto>> AddAsync(TEntity entity);
+        Task<ResponseDto<NoDataDto>> Remove(TEntity entity);
+        Task<ResponseDto<NoDataDto>> Update(TEntity entity);
+    }
+}
