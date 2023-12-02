a7ee5e04469be43c30fad1369346c49973beec2e
Merve <mervezgin@gmail.com>
2023-12-02 17:51:38 +0300

    Added IUserService.cs in Core Layer

---
+++ src/Practise103/Practise103.Core/Services/IUserService.cs
@@ -0,0 +1,16 @@
+﻿using Practise103.Core.Dtos;
+using Practise103.Shared.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practise103.Core.Services
+{
+    public interface IUserService
+    {
+        Task<ResponseDto<UserDto>> CreateUserAsync(CreateUserDto createUserDto);
+        Task<ResponseDto<UserDto>> GetUserByNameAsync(string userName);
+    }
+}
