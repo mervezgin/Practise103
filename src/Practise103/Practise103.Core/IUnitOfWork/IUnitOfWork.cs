f28d2056703c953cfd7da8ef52f345ad7543a090
Merve <mervezgin@gmail.com>
2023-12-02 18:00:47 +0300

    Added IUnitOfWork in Core Layer

---
+++ src/Practise103/Practise103.Core/IUnitOfWork/IUnitOfWork.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practise103.Core.IUnitOfWork
+{
+    public interface IUnitOfWork
+    {
+        Task CommitAsync();
+        void Commit();
+    }
+}
