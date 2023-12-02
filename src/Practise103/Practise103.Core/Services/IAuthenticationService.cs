89aeab88449a2d5ab8f12844d3e9fb49e3bd36bd
Merve <mervezgin@gmail.com>
2023-12-02 17:35:26 +0300

    Added IAuthenticationService.cs in Core Layer

---
+++ src/Practise103/Practise103.Core/Services/IAuthenticationService.cs
@@ -0,0 +1,18 @@
+﻿using Practise103.Core.Dtos;
+using Practise103.Shared.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practise103.Core.Services
+{
+    public interface IAuthenticationService
+    {
+        Task<ResponseDto<TokenDto>> CreateTokenAsync(LoginDto loginDto);
+        Task<ResponseDto<TokenDto>> CreateTokenByRefreshToken(string refreshToken);
+        Task<ResponseDto<NoDataDto>> RevokeRefreshToken(string refreshToken);
+        Task<ResponseDto<ClientTokenDto>> CreateTokenByClient(ClientLoginDto clientLoginDto);
+    }
+}
