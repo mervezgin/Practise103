7ad4ec0f7ee8ea5f8d31d8f8288a7890e0a0e7b0
Merve <mervezgin@gmail.com>
2023-12-05 17:20:36 +0300

    Added ObjectMapper and Update

--- src/Practise103/Practise103.Service/ObjectMapper.cs
+++ src/Practise103/Practise103.Service/ObjectMapper.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using AutoMapper;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +9,15 @@ namespace Practise103.Service
 {
     public static class ObjectMapper
     {
-        private static readonly Lazy<>
+        private static readonly Lazy<IMapper> lazy = new Lazy<IMapper>(() =>
+        {
+            var config = new MapperConfiguration(cfg =>
+            {
+                cfg.AddProfile<DtoMapper>();
+            });
+            return config.CreateMapper();
+        }); 
+
+        public static IMapper Mapper => lazy.Value;
     }
 }
